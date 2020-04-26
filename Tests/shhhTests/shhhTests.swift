7d07702042132657ed5dce1f5fffd0cb4e709d31
Robert Bigelow <rfbigelow@mac.com>
2020-04-26 13:13:57 -0700

    Rename n-tuple test case.

--- Tests/shhhTests/shhhTests.swift
+++ Tests/shhhTests/shhhTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import shhh
 
 final class shhhTests: XCTestCase {
-    func testMixedRadixCount() {
+    func testWhenGeneratingNTuples() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
